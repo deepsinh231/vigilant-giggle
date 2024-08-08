fe8717f5c2fa7ca7ad994c853d2a682099eae2dc
deepsinh231 <deepsinh231@gmail.com>
2024-08-08 09:19:50 +0530

    New

--- src/Component/Mypost.jsx
+++ src/Component/Mypost.jsx
@@ -47,7 +47,7 @@ export default function Mypost() {
                     <Link key={post._id} className={`shadow-md rounded-lg p-4 block ${post.status === "active" ? "bg-white" : "bg-black/10"}`} to={`/post/${post._id}`}>
                         <div className="relative">
                             <h2 className="text-xl font-semibold">{post.title}</h2>
-                            <h2 className="text-xl absolute top-0 right-0 capitalize font-semibold">{post.status}</h2>
+                            <h2 className="text-xl absolute bottom-0 bg-black/35 px-3 py-2 border-none rounded-md right-0 capitalize font-semibold">{post.status}</h2>
                             <p className="text-gray-600">{parse(post.content)}</p>
                             <p className='py-2'>{new Date(post.createdAt).toLocaleString('en-US', { dateStyle: 'medium', timeStyle: 'short' })}</p>
                         </div>
